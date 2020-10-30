2835f355984cf43c105af984923e531be6313577
Aiden Mendez <aidenmendez@gmail.com>
2020-10-30 08:39:08 -0600

    Remove extraneous instance vars

--- lib/author.rb
+++ lib/author.rb
@@ -4,15 +4,15 @@ class Author
 
   def initialize(data)
     @name = data[:first_name] + " " + data[:last_name]
+    @first_name = data[:first_name]
+    @last_name = data[:last_name]
+    @name = "#{@first_name} #{@last_name}"
     @books = []
   end
 
   def write(title, publication_date)
-    author_name = self.name.split(" ")
-    author_first_name = author_name[0]
-    author_last_name = author_name[1]
-    book = Book.new({title: title, publication_date: publication_date, author_first_name: author_first_name, author_last_name: author_last_name})
-    @books << book
+    book = Book.new({title: title, publication_date: publication_date, author_first_name: @first_name, author_last_name: @last_name})
+    books << book
     book
   end
 end
